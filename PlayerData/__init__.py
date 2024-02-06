92bafeff2f486cc5dd7759d0eeb1ca6fb8cf32c3
KyleBD <kylebradleydyer15@outlook.com>
2024-02-05 23:45:22 -0500

    working on init file and user interaction for player data.

--- PlayerData/__init__.py
+++ PlayerData/__init__.py
@@ -1,9 +1,17 @@
+from get_players import get_all_time_player_ids, get_player_ids_for_season, get_season_object_list
+
 if __name__ == "__main__":
+    #Trigger all of the commands to fulfill all data on all players in a current season
+    print("Enter 1 for current season \n")
+    print("Enter 2 for all time \n")
+    print("Enter 3 to pick a season \n")
 
     command = input("Please enter your desired command: ")
     
-    parse_command(command)
     print("\n")
+    parse_command(command)
 
 def parse_command():
+    
+
     return 0
