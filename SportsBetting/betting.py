2ee1ef942db0d0561cb513d56c0869dfb74fcd71
KyleBD <kylebradleydyer15@outlook.com>
2024-03-16 22:51:21 -0400

    Adding sports list to the database and specifically making it pull data for college basketball.

--- SportsBetting/betting.py
+++ SportsBetting/betting.py
@@ -1,52 +1,30 @@
 import requests
 
+# Replace 'YOUR_API_KEY' with your actual API key
+API_KEY = '739cd0db1260420e97dbfb9d12aa5f18'
 
-# An api key is emailed to you when you sign up to a plan
-# Get a free API key at https://api.the-odds-api.com/
-API_KEY = 'YOUR_API_KEY'
+SPORT = 'basketball_ncaab'  # Use 'basketball_ncaab' for college basketball
 
-SPORT = 'upcoming' # use the sport_key from the /sports endpoint below, or use 'upcoming' to see the next 8 games across all sports
+REGIONS = 'us'  # uk | us | eu | au. Multiple can be specified if comma delimited
 
-REGIONS = 'us' # uk | us | eu | au. Multiple can be specified if comma delimited
+MARKETS = 'h2h,spreads'  # Include the markets you want for college basketball
 
-MARKETS = 'h2h,spreads' # h2h | spreads | totals. Multiple can be specified if comma delimited
+ODDS_FORMAT = 'decimal'  # decimal | american
 
-ODDS_FORMAT = 'decimal' # decimal | american
+DATE_FORMAT = 'iso'  # iso | unix
 
-DATE_FORMAT = 'iso' # iso | unix
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-#
 # First get a list of in-season sports
-#   The sport 'key' from the response can be used to get odds in the next request
-#
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-
 sports_response = requests.get(
-       'https://api.the-odds-api.com/v4/sports', 
+    'https://api.the-odds-api.com/v4/sports',
     params={
-        'api_key': KEY
+        'api_key': API_KEY
     }
 )
-
 
 if sports_response.status_code != 200:
     print(f'Failed to get sports: status_code {sports_response.status_code}, response body {sports_response.text}')
 
-else:
-    print('List of in season sports:', sports_response.json())
-
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-#
-# Now get a list of live & upcoming games for the sport you want, along with odds for different bookmakers
-# This will deduct from the usage quota
-# The usage quota cost = [number of markets specified] x [number of regions specified]
-# For examples of usage quota costs, see https://the-odds-api.com/liveapi/guides/v4/#usage-quota-costs
-#
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-
+# Now get a list of live & upcoming games for college basketball along with odds
 odds_response = requests.get(
     f'https://api.the-odds-api.com/v4/sports/{SPORT}/odds',
     params={
@@ -60,12 +38,10 @@ odds_response = requests.get(
 
 if odds_response.status_code != 200:
     print(f'Failed to get odds: status_code {odds_response.status_code}, response body {odds_response.text}')
-
 else:
     odds_json = odds_response.json()
     print('Number of events:', len(odds_json))
     print(odds_json)
-
     # Check the usage quota
     print('Remaining requests', odds_response.headers['x-requests-remaining'])
     print('Used requests', odds_response.headers['x-requests-used'])
---
+++ SportsBetting/sportTypes.py
@@ -0,0 +1,22 @@
+sports_list = [
+    'americanfootball_nfl', 'americanfootball_ncaaf',
+    'baseball_mlb',
+    'basketball_nba', 'basketball_ncaab', 'basketball_nba_women',
+    'boxing',
+    'cricket',
+    'cycling',
+    'darts',
+    'esports_csgo', 'esports_dota2', 'esports_lol',  # And more for various games
+    'soccer_epl',  # Example for English Premier League, multiple leagues available
+    'golf',
+    'handball',
+    'icehockey_nhl', 'icehockey_ncaa',
+    'horseracing',
+    'mma',
+    'motorsports_f1', 'motorsports_nascar', 'motorsports_motogp',
+    'rugbyunion', 'rugbyleague',
+    'snooker',
+    'tabletennis',
+    'tennis',
+    'volleyball'
+]
